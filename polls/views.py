07c29ce5af0d935c75d540b410a82f8cc49b4cd1
vigack <vigack@gmail.com>
2018-01-07 15:56:50 +0800

    add some tests

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.utils import timezone
+from .models import Question
 
-def index(request):
-    return HttpResponse("Hell, world. You are in the polls index.")
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting at the question %s." % question_id)
